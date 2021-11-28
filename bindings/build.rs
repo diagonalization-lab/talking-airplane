5d301e0f8addb0c6c31806cc23ec65bd4842ec2c
Diagonalization <diagonalization@fastmail.com>
2021-11-28 15:53:28 -0500

    Removed bindings sub-project

--- bindings/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    windows::build!(
-        Windows::Media::SpeechSynthesis::SpeechSynthesizer,
-        Windows::Media::SpeechSynthesis::SpeechSynthesisStream,
-        Windows::Media::Playback::MediaPlayer,
-        Windows::Media::Core::MediaSource,
-    );
-}
